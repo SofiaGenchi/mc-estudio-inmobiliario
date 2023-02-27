564b9183b92f817280c5b46eb2bfc10a82a5581c
Sofia Genchi <sofigen16@gmail.com>
2023-02-27 13:47:14 -0300

    creando seccion de propiedades

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import './header.css'
-import 'boxicons'
+import React from "react";
+import "./header.css";
+import "boxicons";
 
 const Header = () => {
   return (
     <header>
       <nav className="nav container">
         <a href="#" className="nav__logo">
-          MC Estudio Inmobiliario <box-icon name='home'></box-icon>
+          MC Estudio Inmobiliario <box-icon name="home"></box-icon>
         </a>
 
         <div className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
               <a href="#" className="nav__link active-link">
-              <box-icon type='solid' name='home-alt-2'></box-icon>
-              <span>Inicio</span>
+                <box-icon type="solid" name="home-alt-2"></box-icon>
+                <span>Inicio</span>
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#" className="nav__link active-link">
+                <box-icon name='building-house'></box-icon>
+                <span>Propiedades</span>
               </a>
             </li>
 
             <li className="nav__item">
               <a href="#" className="nav__link">
-              <box-icon name='award'></box-icon>
-              <span>Valoración</span>
+                <box-icon name="award"></box-icon>
+                <span>Valoración</span>
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#" className="nav__kink">
-              <box-icon name='phone'></box-icon>
-              <span>Contacto</span>
+              <a href="#" className="nav__link">
+                <box-icon name="phone"></box-icon>
+                <span>Contacto</span>
               </a>
             </li>
           </ul>
         </div>
-        <box-icon name='moon' className='change-theme' id='theme-button'></box-icon>
+        <box-icon
+          name="moon"
+          className="change-theme"
+          id="theme-button"
+        ></box-icon>
       </nav>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="main">
+      <section className="home section" id="home">
+        <div className="home__container container grid">
+          <div className="home__data">
+            <h1 className="home__title">
+              Descubre <br /> la propiedad <br /> más adecuada
+            </h1>
 
-export default Home
+            <p className="home__description">
+              Encuentre una variedad de propiedades que se adapten a usted muy
+              facilmente, olvidese de todas las dificultades para encontrar un
+              hogar.
+            </p>
+
+            {/* <form action="" className="home__search">
+                        <box-icon className='navigation' name='navigation'></box-icon>
+                        <input type="search" placeholder='Buscar por localidad...' className="home__search-input" />
+                        <button className="button">Buscar</button>
+                    </form> */}
+
+            <div className="home__value">
+              <div>
+                <h2 className="home__value-number">
+                  9k <span>+</span>
+                </h2>
+                <span className="home__value-description">
+                  Producto <br />
+                  Premium
+                </span>
+              </div>
+              <div>
+                <h2 className="home__value-number">
+                  2K <span>+</span>
+                </h2>
+                <span className="home__value-description">
+                  Cliente <br />
+                  Feliz
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="home__images">
+          <div className="home__orbe">
+                <div className="home__img">
+                  <img src="../img/homeimg.jpg" alt="" />
+                </div>
+              </div>
+          </div>
+        </div>
+      </section>
+      
+
+      <section className="popular section" id="popular">
+        <div className="container">
+            <span className="section__subtitle">Mejor elección</span>
+            <h2 className="section__title">Casas Populares <span>.</span></h2>
+
+            <div className="popular__container swiper">
+                <div className="swiper-wrapper">
+                    <article className="popular__card swiper-slide">
+                        <img src="" alt="" className="popular__img" />
+                        <div className="popular__data">
+                            <h2 className="popular__price"><span>$$</span></h2>
+                            <h3 className="popular__title"></h3>
+                            <p className="popular__description"></p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
