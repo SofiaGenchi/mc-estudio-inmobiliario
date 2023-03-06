67051196323b5381be901a4f2cd9950ce1f49dc6
Sofia Genchi <sofigen16@gmail.com>
2023-03-06 20:37:53 -0300

    swiper

--- src/components/Propiedades/Propiedades.jsx
+++ src/components/Propiedades/Propiedades.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination } from 'swiper';
+import data from './data.json';
+import 'boxicons';
 
-const Propiedades = () => {
-    const [popular, setPopular] = useState([]);
+import './propiedades.css';
 
-    useEffect(() => {
-        async function fetchPopulars() {
-            const requestURL = 'public/json/datos.json';
-            const response = await fetch(requestURL);
-            const propiedadesText = await response.text();
-            const misPropiedades = JSON.parse(propiedadesText);
-            setPopular(misPropiedades.popular);
-        }
-        fetchPopulars();
-    }, []);
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+
+
+function Propiedades() {
 
   return (
     <section className="popular section" id="popular">
@@ -26,12 +24,20 @@ const Propiedades = () => {
             <Swiper className="popular__container swiper"
                 modules={[Pagination, Navigation]}
                 spaceBetween={20}
-                slidesPerView={1}
                 pagination={{clickable: true}}
+                loop={true}
+                breakpoints={{
+                    534: {
+                        slidesPerView: 3
+                    },
+                    320: {
+                        slidesPerView: 1
+                    }
+                }}
             >
-                {popular.map((propiedad) => (
-                    <div className="swiper-wrapper" key={propiedad.nombre}>
-                    <SwiperSlide className='popular__Card swiper-slide'>
+                {data.propiedades.map((propiedad, index) => (
+                    <div className="swiper-wrapper" key={index}>
+                    <SwiperSlide className='popular__card swiper-slide'>
                         <img src={propiedad.imagen} alt={propiedad.nombre} className="popular__img" />
 
                         <div className="popular__data">
@@ -49,7 +55,7 @@ const Propiedades = () => {
                         </div>
                     </SwiperSlide>
                     </div>
-                ))}
+                ))};
             </Swiper>
         </div>
     </section>
