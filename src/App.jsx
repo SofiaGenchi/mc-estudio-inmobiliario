ebfefb1638da0e417331028727ca0e29ccebb73b
Sofia Genchi <sofigen16@gmail.com>
2023-03-06 15:53:44 -0300

    reiniciando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      
     </>
   );
 }
