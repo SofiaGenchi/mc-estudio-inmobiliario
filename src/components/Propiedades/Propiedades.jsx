e0616f4361f10f0cd601623a7badb4d5b50a04aa
Sofia Genchi <92803854+SofiaGenchi@users.noreply.github.com>
2024-05-24 18:21:43 -0300

    arreglo swipper

--- src/components/Propiedades/Propiedades.jsx
+++ src/components/Propiedades/Propiedades.jsx
@@ -80,52 +80,50 @@ return (
       </button>
     </div>
         <Swiper className="popular__container swiper"
-            modules={[Navigation]}
-            spaceBetween={22}
-            // pagination={{clickable: true}}
-            loop={true}
-            breakpoints={{
-                534: {
-                    slidesPerView: 3
-                },
-                320: {
-                    slidesPerView: 1
-                }
-            }}
-            navigation={{
-                prevEl: '.prev',
-                nextEl: '.next',
-            }}
-        >
-            {propiedadesFiltradas.map((propiedad, index) => (
-                <div className="swiper-wrapper" key={index}>
-                <SwiperSlide className='popular__card swiper-slide'>
-                        <a href={propiedad.link} target="_blank" rel="noopener noreferrer">
-                            <img src={propiedad.imagen} alt={propiedad.descripcion} className="popular__img" />
-
+    modules={[Navigation]}
+    spaceBetween={22}
+    loop={true}
+    breakpoints={{
+        534: {
+            slidesPerView: 3
+        },
+        320: {
+            slidesPerView: 1
+        }
+    }}
+    navigation={{
+        prevEl: '.prev',
+        nextEl: '.next',
+    }}
+>
+    <div className="swiper-wrapper">
+        {propiedadesFiltradas.map((propiedad, index) => (
+            <SwiperSlide className='popular__card swiper-slide' key={index}>
+                <a href={propiedad.link} target="_blank" rel="noopener noreferrer">
+                    <img src={propiedad.imagen} alt={propiedad.descripcion} className="popular__img" />
+                </a>
+                <div className="popular__data">
+                    <h2 className="popular__price">
+                        {propiedad.precio}
+                    </h2>
+                    <h3 className="popular__title">
+                        <a className='nombre-propiedad' href={propiedad.link} target="_blank" rel="noopener noreferrer">
+                            {propiedad.nombre}
                         </a>
-                    <div className="popular__data">
-                        <h2 className="popular__price">
-                            {propiedad.precio}
-                        </h2>
-                        <h3 className="popular__title">
-                            <a className='nombre-propiedad' href={propiedad.link} target="_blank" rel="noopener noreferrer">
-                                {propiedad.nombre}
-                            </a>
-                        </h3>
-                        <p className="popular__description">
-                            {propiedad.descripcion}
-                        </p>
-                    </div>
-                </SwiperSlide>
+                    </h3>
+                    <p className="popular__description">
+                        {propiedad.descripcion}
+                    </p>
                 </div>
-            ))}
-            <div className='next-prev'>
-                <div className="prev swiper-prev"><box-icon name='chevron-left' className=""></box-icon></div>
-                <div className="next swiper-next"><box-icon name='chevron-right' className=""></box-icon></div>
-            </div>
-            
-        </Swiper>
+            </SwiperSlide>
+        ))}
+    </div>
+    <div className='next-prev'>
+        <div className="prev swiper-prev"><box-icon name='chevron-left' className=""></box-icon></div>
+        <div className="next swiper-next"><box-icon name='chevron-right' className=""></box-icon></div>
+    </div>
+</Swiper>
+
     </div>
 </section>
   )
